c3905d4437d29367098fdd72d65c980784899047
Robson <robson.lsilva36@gmail.com>
2024-06-13 23:12:23 -0300

    Conclusão do desafio, utilizado a classe Scanner para ler os dados digitados, feito parse dos dados de saldo e numero da conta e ao final exibir dados concatenados.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o nome do cliente !");
+        String nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da agência !");
+        String agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o numero da conta !");
+        int numero = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Por favor, digite o saldo do cliente !");
+        double saldo = Double.parseDouble(sc.nextLine());
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %,.2f", nomeCliente, agencia, numero, saldo);
+
+        sc.close();
     }
 }
