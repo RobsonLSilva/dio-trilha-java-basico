ba928f432fdd945e66a9783f4517fcb60cd9e394
Robson <robson.lsilva36@gmail.com>
2024-07-28 23:17:03 -0300

    Java RESTful API criada para o BootCamp-Java-Santander-2024

--- untitled/src/Main.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class Main {
-    public static void main(String[] args) {
-        LoginAppGUI app = new LoginAppGUI();
-        SwingUtilities.invokeLater(() -> app.createAndShowGUI());
-    }
-}
