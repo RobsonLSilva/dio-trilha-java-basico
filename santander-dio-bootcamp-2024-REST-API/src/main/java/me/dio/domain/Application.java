f4b08f877466e34e1c9f6947df4be3f163b15df3
Robson <robson.lsilva36@gmail.com>
2024-07-30 00:59:17 -0300

    Criação das entidades, seus relacionamentos, criação do arquivo de configuração para dev com banco de dados na memória "H2".

--- santander-dio-bootcamp-2024-REST-API/src/main/java/me/dio/domain/Application.java
+++ santander-dio-bootcamp-2024-REST-API/src/main/java/me/dio/domain/Application.java
@@ -1,13 +1,12 @@
-package me.dio.santander_dio_bootcamp_2024_REST_API;
+package me.dio.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderDioBootcamp2024RestApiApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderDioBootcamp2024RestApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
